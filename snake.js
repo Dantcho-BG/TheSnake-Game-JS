8b5457c1388f4348b0caee8a8106fe60bbd530a6
Yordan Hristov (Dantcho) <dantcho.bg@gmail.com>
2019-02-07 22:40:01 +0000

    Added more functionality to the script (Still not done)
    Added a function that draws the grid and started working on a function that will move it forward. Also added a function that calls the draw function every
    second.

--- snake.js
+++ snake.js
@@ -2,8 +2,6 @@
 var canvas = document.getElementById("gameCanvas");
 //Rendering context
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 10, 10);
 
 //Get canvas width and height
 var canvasWidth = canvas.width;
@@ -17,7 +15,7 @@ var gridSizeHeight = parseInt(canvasHeight / 10);
 console.log(gridSizeWidth + " - Grid Width");
 console.log(gridSizeHeight + " - Grid Height");
 
-//Create grid array
+//Create an empty grid array
 var grid = new Array;
 
 counterWidth = 0;
@@ -49,6 +47,80 @@ while (counterHeight < gridSizeHeight) {
 
 }
 
-//Show the grid array in the console
+//Get the middle of the grid
+gridMiddleX = parseInt(gridSizeWidth / 2);
+gridMiddleY = parseInt(gridSizeHeight / 2);
+
+//Set the initial position of the snake
+grid[gridMiddleY][gridMiddleX] = 1;
+
+snakeSecondPartX = gridMiddleX - 1;
+snakeThirdPartX = snakeSecondPartX - 1;
+
+grid[gridMiddleY][snakeSecondPartX] = 1;
+grid[gridMiddleY][snakeThirdPartX] = 1;
+
+//Show the middle of the grind in the console
+console.log(gridMiddleX + " - Grid X Middle");
+console.log(gridMiddleY + " - Grid Y Middle");
+
+//Draw the grid before the interval code runs the first time
+draw ();
+
+setInterval(function () {
+    
+    draw ();
+
+}, 1000);
+
+function moveSnakeForward () {
+
+    
+
+}
+
+//Show the grid array in the console before drawing it (Used for debuging)
 //console.log(grid);
+
+//Draw the grid
+
+function draw () {
+
+    height = 0;
+    width = 0;
+
+    while (height < gridSizeHeight) {
+
+        width = 0;
+
+        while (width < gridSizeWidth) {
+
+            if (grid[height][width] == 0) {
+
+                posY = height * 10;
+                posX = width * 10;
+
+                ctx.fillStyle = "#FFFFFF";
+                ctx.fillRect(posX, posY, 10, 10);
+
+            }
+            else if (grid[height][width] == 1) {
+
+                posY = height * 10;
+                posX = width * 10;
+
+                ctx.fillStyle = "#FF0000";
+                ctx.fillRect(posX, posY, 10, 10);
+
+            }
+
+            width++;
+
+        }
+
+        height++;
+
+    }
+
+}
 
